6774ec2c25e2d220bdba5b4d1bfd8bca61e9a012
Elizabeth Sham <esham00@stuy.edu>
2018-10-10 02:01:18 -0400

    Wrote point class, distance is not working but distanceTo is

--- Driver.java
+++ Driver.java
@@ -1,1 +1,9 @@
-
+public class Driver {
+  public static void main(String[] args) {
+    Point sad = new Point(0,1);
+    Point happy = new Point(0,0);
+    System.out.println(sad.distanceTo(happy));
+    System.out.println(sad);
+    System.out.println(happy);
+  }
+}
--- Point.java
+++ Point.java
@@ -1,1 +1,26 @@
-
+public class Point {
+  private double x, y;
+  public Point(double x1, double y1) {
+    x = x1;
+    y = y1;
+  }
+  public Point(Point p) {
+    x = p.x;
+    y = p.y;
+  }
+  public double getX() {
+    return x;
+  }
+  public double getY() {
+    return y;
+  }
+  public double distanceTo(Point p) {
+    return Math.sqrt(Math.pow((this.x - p.x), 2) + Math.pow((this.y - p.y), 2));
+  }
+  public double distance(Point k, Point p) {
+    return Math.sqrt(Math.pow((k.x - p.x), 2) + Math.pow((k.y - p.y), 2));
+  }
+  public String toString() {
+    return "(" + x + "," + y + ")";
+  }
+}
